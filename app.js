04c28c93c727834243a91208af74a4e4e0f06d03
yokesh-a <bossyokesh3377@gmail.com>
2020-12-09 13:52:33 +0530

    yokesh

--- app.js
+++ app.js
@@ -4,12 +4,29 @@ const path = require('path');
 
 const app = express();
 
-app.use(express.static(path.join(__dirname,'D6')));
+app.use(express.static(path.join(__dirname,'agro')));
 
 app.get('/',function(req,res){
-    res.sendFile(path.join(__dirname,'D6/test.html'));
+    res.sendFile(path.join(__dirname,'agro/index.html'));
 })
 
+
+const { networkInterfaces } = require('os');
+const nets = networkInterfaces();
+const results = Object.create(null); // or just '{}', an empty object
+for (const name of Object.keys(nets)) {
+    for (const net of nets[name]) {
+        // skip over non-ipv4 and internal (i.e. 127.0.0.1) addresses
+        if (net.family === 'IPv4' && !net.internal) {
+            if (!results[name]) {
+                results[name] = [];
+            }
+            results[name].push(net.address);
+        }
+    }
+}
+
+
 app.listen(80,function(req,res){
-    console.log('start server');
+    console.log('Server Starts in','http://'+results['Local Area Connection'][0]);
 });
--- save.bat
+++ save.bat
@@ -1,4 +1,4 @@
 @ECHO OFF
 git add .
 git commit -am "yokesh"
-git push --set-upstream vps master
+git push -u origin main
